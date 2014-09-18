de8e56223934d8bd14d1c4b44c9a5d6993eb744a
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2014-09-18 16:26:11 -0500

    Change the recipient from the contact mails

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,6 +1,6 @@
 class NotificationsMailer < ActionMailer::Base
 
-  default :from => "noreply@staunchrobots.com", to: "juan.giraldo@staunchrobots.com"
+  default :from => "noreply@staunchrobots.com", to: "todd.morrill@staunchrobots.com"
 
   def new_message(message)
     @message = message
