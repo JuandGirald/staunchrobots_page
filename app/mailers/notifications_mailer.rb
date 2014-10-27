ad75a62342fe045f843dd8665f1b505f29415272
Derek Scruggs <me@derekscruggs.com>
2014-10-27 09:14:53 -0500

    Added derek to contact email

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,6 +1,6 @@
 class NotificationsMailer < ActionMailer::Base
 
-  default :from => "noreply@staunchrobots.com", to: "todd.morrill@staunchrobots.com"
+  default :from => "noreply@staunchrobots.com", to: "todd.morrill@staunchrobots.com", cc: 'derek.scruggs@staunchrobots.com'
 
   def new_message(message)
     @message = message
