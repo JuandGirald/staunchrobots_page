7527e4d4d996a5a6e652e516289acaae9291be22
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2015-01-26 15:56:26 -0500

    Project Pick and Sort (dh)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113192336) do
+ActiveRecord::Schema.define(version: 20150126203710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20150113192336) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "tech_stack"
+    t.integer  "order"
   end
 
   create_table "skills", force: true do |t|
