1f49e478c5c2da7608861e2767c46e75d0dc8b11
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2015-02-16 11:39:23 -0500

    Redirect to the last page when login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  after_filter :store_location
   before_filter :update_sanitized_params, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
@@ -34,11 +35,26 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def auth_user
-    redirect_to login_path unless user_signed_in?
+  # def auth_user
+  #   redirect_to login_path unless user_signed_in?
+  # end
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get? 
+    if (request.path != "/users/sign_in" &&
+        request.path != "/login" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
   end
 
-  def after_sign_in_path_for(resource)
-    cvs_path
-  end
+def after_sign_in_path_for(resource)
+  session[:previous_url] || cvs_path
+end
 end
