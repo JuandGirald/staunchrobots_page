c2244baf2e186485f7cb9f2fbc7b32189c6ce9c7
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2015-02-20 10:20:11 -0500

    Change redirecting function when login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,22 +40,22 @@ class ApplicationController < ActionController::Base
   #   redirect_to login_path unless user_signed_in?
   # end
 
-  def store_location
-    # store last url - this is needed for post-login redirect to whatever the user last visited.
-    return unless request.get? 
-    if (request.path != "/users/sign_in" &&
-        request.path != "/login" &&
-        request.path != "/users/sign_up" &&
-        request.path != "/users/password/new" &&
-        request.path != "/users/password/edit" &&
-        request.path != "/users/confirmation" &&
-        request.path != "/users/sign_out" &&
-        !request.xhr?) # don't store ajax calls
-      session[:previous_url] = request.fullpath 
-    end
-  end
+  # def store_location
+  #   # store last url - this is needed for post-login redirect to whatever the user last visited.
+  #   return unless request.get? 
+  #   if (request.path != "/users/sign_in" &&
+  #       request.path != "/login" &&
+  #       request.path != "/users/sign_up" &&
+  #       request.path != "/users/password/new" &&
+  #       request.path != "/users/password/edit" &&
+  #       request.path != "/users/confirmation" &&
+  #       request.path != "/users/sign_out" &&
+  #       !request.xhr?) # don't store ajax calls
+  #     session[:previous_url] = request.fullpath 
+  #   end
+  # end
 
-def after_sign_in_path_for(resource)
-  session[:previous_url] || cvs_path
-end
+  def after_sign_in_path_for(resource)
+    cvs_path
+  end
 end
