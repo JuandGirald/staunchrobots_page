9a5e185dafac3ac1897ccb6b35ce48e26f923233
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2015-02-20 10:26:19 -0500

    Fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   after_filter :store_location
-  before_filter :update_sanitized_params, if: :devise_controller?
+  # before_filter :update_sanitized_params, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     if current_user
